06e299665d46aa31aa5369f6bb5e2148f3fc0ef2
Peter Kinmond <peterkinmond@gmail.com>
2019-01-12 07:36:52 -0800

    Write machine language Multiply function

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,73 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Pseudo-code
+//  n = R0
+//  m = R1
+//  i = 1
+//  sum = 0
+// LOOP:
+//  if i > n goto STOP
+//    sum = sum + m
+//    i = i + 1
+//    goto LOOP
+// STOP:
+//  R2 = sum
+
+// Real code
+  //  n = R0
+  @0
+  D=M
+  @n
+  M=D
+
+  //  m = R1
+  @1
+  D=M
+  @m
+  M=D
+
+  //  i = 1
+  @i
+  M=1
+
+  //  sum = 0
+  @sum
+  M=0
+
+// LOOP:
+(LOOP)
+  //  if i > n goto STOP
+  @i
+  D=M
+  @n
+  D=D-M
+  @STOP
+  D;JGT
+
+  //    sum = sum + m
+  @m
+  D=M
+  @sum
+  M=D+M
+
+  //    i = i + 1
+  @i
+  M=M+1
+
+  //    goto LOOP
+  @LOOP
+  0;JMP
+
+// STOP:
+(STOP)
+  //  R2 = sum
+  @sum
+  D=M
+  @2
+  M=D
+
+(END)
+  @END
+  0;JMP
