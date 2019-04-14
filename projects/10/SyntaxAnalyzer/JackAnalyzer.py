628d90229ccd9dde4c61315a461ee7c6650d6a5c
Peter Kinmond <peterkinmond@gmail.com>
2019-04-14 16:44:07 -0700

    Add basic file handling for Jack Analyzer
    * Handles either a single Jack file or a all Jack files in a directory.
    THis functionality is very close to Project 8, which converted VM files
    into assembly files, so most of the code here I just repurposed from
    what I wrote in Project 8's VM translator.
    * I've left the specific implementation of what to do with the files for
    later until I've written the Jack tokenizer and compilation engine.

--- projects/10/SyntaxAnalyzer/JackAnalyzer.py
+++ projects/10/SyntaxAnalyzer/JackAnalyzer.py
@@ -1,2 +1,33 @@
+import glob
+import os
+import sys
+
 def main():
-    pass
+    path = sys.argv[1]
+    jack_files = []
+    output_filepath = ''
+    is_directory = False
+
+    if os.path.isfile(path):
+        jack_files = [path]
+        output_filepath = os.path.splitext(path)[0] + ".xml"
+    elif os.path.isdir(path):
+        is_directory = True
+        if path.endswith('/'):
+            path = path[:-1]
+        os.chdir(path)
+        for file in glob.glob("*.jack"):
+            jack_files.append(file)
+        output_filepath = os.path.split(path)[1] + ".xml"
+    else:
+        print("Path {} does not exist. Exiting...".format(path))
+        sys.exit()
+
+    for jack_file in jack_files:
+        print('Loading file: ' + jack_file)
+        # TODO: analyze each file
+
+    print('Closing file: ' + jack_file)
+
+if __name__ == '__main__':
+    main()
